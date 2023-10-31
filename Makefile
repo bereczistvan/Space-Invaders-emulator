ab565f3c181e061be50a1d10327cac29c0c26088
bereczistvan <50058145+bereczistvan@users.noreply.github.com>
2023-10-31 20:03:22 +0100

    Move CPU emulation to 8080cpu.c and 8080cpu.h

--- Makefile
+++ Makefile
@@ -2,11 +2,14 @@
 
 all: invaders
 
-invaders: main.o
-	gcc -o invaders main.o -lmingw32 -lSDL2main -lSDL2 -mwindows
+invaders: main.o cpu8080.o
+	gcc -o invaders main.o cpu8080.o -lmingw32 -lSDL2main -lSDL2 -mwindows
 
 main.o: main.c
 	gcc -Wall -c main.c
+
+cpu8080.o: cpu8080.c
+	gcc -Wall -c cpu8080.c
 
 clean:
 	del *.o
---
+++ cpu8080.h
@@ -0,0 +1,79 @@
+#ifndef CPU8080_H
+#define CPU8080_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+
+// Status registers
+typedef struct Flags
+{
+    uint8_t c: 1;           // Carry
+    uint8_t undefiend1: 1;
+    uint8_t p: 1;           // Parity
+    uint8_t undefined3: 1;
+    uint8_t ac:1;           // Auxiliary carry
+    uint8_t undefined5: 1;
+    uint8_t z: 1;           // Zero
+    uint8_t s: 1;           // Sign
+} Flags;
+
+typedef struct State8080
+{
+    union
+    {
+        struct
+        {
+            uint8_t rl;
+            uint8_t rh;
+            uint8_t re;
+            uint8_t rd;
+            uint8_t rc;
+            uint8_t rb;
+            struct Flags rf;
+            uint8_t ra;
+        };
+        struct
+        {
+            uint16_t rhl;
+            uint16_t rde;
+            uint16_t rbc;
+            uint16_t psw;
+        };
+    };
+    uint16_t sp;            // Stack Pointer
+    uint16_t pc;            // Program Counter
+    uint8_t *memory;
+    uint8_t inte;           // Interrupt Enabled
+    uint8_t *portsin;
+    uint8_t *portsout;
+    uint16_t rshift;
+} State8080;
+
+bool parity(uint16_t res);
+
+void set_flags_8(State8080* state, uint8_t res);
+
+void set_flags_16_cy(State8080* state, uint16_t res);
+
+void set_flags_logic(State8080* state);
+
+void add8(State8080* state, uint8_t a, uint8_t b);
+
+void sub8(State8080* state, uint8_t b);
+
+void cmp(State8080* state, uint8_t b);
+
+void inr(State8080* state, uint8_t* reg);
+
+void dcr(State8080* state, uint8_t* reg);
+
+void UnimplementedInstruction(State8080* state);
+
+State8080* Init8080(void);
+
+void Emulate8080Op(State8080* state);
+
+#endif
